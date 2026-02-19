733a0af4b5540be12766aed3ba115be6104e94d7
willianbydio-glitch <willianbydio@gmail.com>
2026-02-19 20:52:55 -0300

    Add API handler for team statistics

---
+++ api/estatisticas.js
@@ -0,0 +1,18 @@
+import fetch from "node-fetch";
+
+const API_KEY = "3f052ff910b32e4aefa8fe5fd1063387";
+
+export default async function handler(req, res) {
+  const { team, league, season } = req.query;
+
+  try {
+    const response = await fetch(
+      `https://v3.football.api-sports.io/teams/statistics?team=${team}&league=${league}&season=${season}`,
+      { headers: { "x-apisports-key": API_KEY } }
+    );
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+}
