b9fed5b43a3e7e66a2e2d99cbea07cb446ec5cb7
willianbydio-glitch <willianbydio@gmail.com>
2026-02-20 09:14:54 -0300

    Update jogos.js

--- api/jogos.js
+++ api/jogos.js
@@ -1,19 +1,20 @@
 // api/jogos.js
 const fetch = require("node-fetch");
 
-const API_KEY = "3f052ff910b32e4aefa8fe5fd1063387";
+const API_KEY = "csjzMBVSuCm5M0JKNGm38UIiPX5tTUHuaRa5sSmjzFXKrvR9E0kzcbysmftt";
+const BASE_URL = "https://v3.football.api-sports.io";
 
-module.exports = async (req, res) => {
-  const date = req.query.date || new Date().toISOString().split("T")[0];
-
+export default async function handler(req, res) {
   try {
-    const response = await fetch(
-      `https://v3.football.api-sports.io/fixtures?date=${date}`,
-      { headers: { "x-apisports-key": API_KEY } }
-    );
+    const date = req.query.date;
+    const response = await fetch(`${BASE_URL}/fixtures?date=${date}`, {
+      method: "GET",
+      headers: { "x-apisports-key": API_KEY }
+    });
+
     const data = await response.json();
     res.status(200).json(data);
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
-};
+}
