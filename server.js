21ad729285bd7ccc6414c036fb6011dcbb6aff90
willianbydio-glitch <willianbydio@gmail.com>
2026-02-24 11:14:42 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 const express = require("express");
-const path = require("path");
+const cors = require("cors");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/api/jogos", require("./api/jogos"));
 app.use("/api/estatisticas", require("./api/estatisticas"));
+app.use("/api/analise", require("./api/analise"));
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log("GolBetPro Elite Ultra rodando");
+  console.log("🚀 GolBetPro Backend rodando na porta " + PORT);
 });
